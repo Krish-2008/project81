95a15716604ed1c6f880f1a3bbdab300c9fe0f13
Krish-2008 <89650110+Krish-2008@users.noreply.github.com>
2022-06-13 20:32:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View,StyleSheet,Text} from 'react-native'
+import Createpost from "./Screens/Createpost";
+import Feeds from "./Screens/Feeds";
+import profile from "./Screens/profile";
+
+
+export default function App(){
+    <View style={styles.Container}>
+        
+    
+    </View>
+}
